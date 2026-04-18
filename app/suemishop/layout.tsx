48a20a0fed2858ec64c188dcb75709e46db56f06
clairenakilacc <clairenakilacc@gmail.com>
2026-04-18 22:56:17 +0800

    fix:fixing header/add truncate on text

--- app/suemishop/layout.tsx
+++ app/suemishop/layout.tsx
@@ -360,7 +360,9 @@ export default function DashboardLayout({
         }}
       >
         <header className="bg-light border-bottom p-3 d-flex justify-content-between align-items-center">
-          <span className="fw-semibold text-secondary">Hi, {userName}</span>
+          <span className="fw-semibold text-secondary text-truncate">
+            Hi, {userName}
+          </span>
 
           <button className="btn btn-logout btn-sm" onClick={handleLogout}>
             Logout
