48c64c0beb2950d1421706d6a655669b0238024c
clairenakilacc <clairenakilacc@gmail.com>
2026-04-20 00:29:08 +0800

    fix: misc

--- app/suemishop/items/page.tsx
+++ app/suemishop/items/page.tsx
@@ -208,7 +208,7 @@ export default function SoldItemsPage() {
           <ExportButton data={items} filename="items.csv" /> */}
           <ExportButton
             data={items}
-            table="items" // 🔥 EXPORT ALL FROM DB
+            table="items"
             headersMap={{
               "Created At": (row: any) => {
                 if (!row.created_at) return "";
